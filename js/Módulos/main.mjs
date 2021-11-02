6e6b1ee44f9986d4ecb9d6161f892c67900bb31a
Gangorra <felipegangorra@gmail.com>
2021-11-02 14:13:44 -0300

    atualização

---
+++ js/Módulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funções.mjs";
+
+console.log(mostraIdade("Felipe", 21));
+console.log(mostraCidade("Felipe", "Caturité"));
+console.log(mostraHobby("Felipe", "Jogar"));
